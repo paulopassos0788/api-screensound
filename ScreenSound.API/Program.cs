d4da357adeb9640a92bfdf82317437f68b1ac7ed
Paulo Cesar Passos Pereira <paulo.passos1@gmail.com>
2025-06-03 08:50:52 -0300

    criando uma api minima
    - Criar métodos de consulta através do verbo HTTP GET;
    
    - Implementar uma API REST e criar seus endpoints;
    
    - Usar injeção de dependências para utilizar recursos;
    
    - Criar os endpoints para atualizar e excluir dados.

--- ScreenSound.API/Program.cs
+++ ScreenSound.API/Program.cs
@@ -1,15 +1,65 @@
+using Microsoft.AspNetCore.Mvc;
 using ScreenSound.Banco;
 using ScreenSound.Modelos;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ScreenSoundContext>();
+builder.Services.AddTransient<DAL<Artista>>();
+
 builder.Services.Configure<Microsoft.AspNetCore.Http.Json.JsonOptions>(options => options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.MapGet("/Artistas", ([FromServices] DAL<Artista> dal) =>
 {
-    var dal = new DAL<Artista>(new ScreenSoundContext());
-    return dal.Listar();
+    return Results.Ok(dal.Listar());
+});
+
+app.MapGet("/Artistas/{nome}", ([FromServices] DAL < Artista > dal, string nome) =>
+{
+    var artista = dal.RecuperarPor(a => a.Nome.ToUpper().Equals(nome.ToUpper()));
+
+    if (artista == null)
+    {
+        return Results.NotFound($"Artista {nome} não encontrado.");
+    }
+
+    return Results.Ok(artista);
+});
+
+app.MapPost("/Artistas", ([FromServices] DAL < Artista > dal, [FromBody] Artista artista) =>
+{
+    dal.Adicionar(artista);
+    return Results.Ok(artista);
+});
+
+app.MapDelete("/Artistas/{id}", ([FromServices] DAL<Artista> dal, int id) =>
+{
+    var artista = dal.RecuperarPor(a => a.Id.Equals(id));
+    if (artista == null)
+    {
+        return Results.NotFound($"Artista {artista.Nome} não encontrado.");
+    }
+    dal.Deletar(artista);
+    return Results.NoContent();
+});
+
+app.MapPut("/Artistas", ([FromServices] DAL<Artista> dal, [FromBody] Artista artista) => 
+{
+    var artistaAtualizar = dal.RecuperarPor(a => a.Id == artista.Id);
+
+    if (artistaAtualizar is null)
+    {
+        return Results.NotFound();
+    }
+
+    artistaAtualizar.Nome = artista.Nome;
+    artistaAtualizar.Bio = artista.Bio;
+    artistaAtualizar.FotoPerfil = artista.FotoPerfil;
+
+    dal.Atualizar(artistaAtualizar);
+    return Results.Ok();
 });
 
 app.Run();
--- ScreenSound.Shared.Modelos/Modelos/Artista.cs
+++ ScreenSound.Shared.Modelos/Modelos/Artista.cs
@@ -11,6 +11,10 @@ public class Artista
         FotoPerfil = "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909_1280.png";
     }
 
+    public Artista()
+    {
+    }
+
     public string Nome { get; set; }
     public string FotoPerfil { get; set; }
     public string Bio { get; set; }
